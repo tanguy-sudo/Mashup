45fe4b0866f6c5999b619f64ea1bed3adcf5a437
tanguy <tanguy.jouvin@hotmail.com>
2022-11-07 16:13:56 +0100

    add julien

--- internalCRM/src/main/java/fr/univ/angers/InternalClientRepository.java
+++ internalCRM/src/main/java/fr/univ/angers/InternalClientRepository.java
@@ -19,7 +19,7 @@ public class InternalClientRepository {
 
         ws_crm.InternalClient internalClient = new ws_crm.InternalClient();
         internalClient.setFirstAndLastName("tanguy,jouvin");
-        internalClient.setAnnualRevenue(36000.0);
+        internalClient.setAnnualRevenue(38000.0);
         internalClient.setPhone("0123456789");
         internalClient.setStreet("19 rue du laurier");
         internalClient.setPostalCode("49000");
@@ -32,7 +32,7 @@ public class InternalClientRepository {
 
         ws_crm.InternalClient internalClient2 = new ws_crm.InternalClient();
         internalClient2.setFirstAndLastName("guillaume,grenon");
-        internalClient2.setAnnualRevenue(38000.0);
+        internalClient2.setAnnualRevenue(36000.0);
         internalClient2.setPhone("0458569545");
         internalClient2.setStreet("19 rue de bretagne");
         internalClient2.setPostalCode("49000");
@@ -42,6 +42,20 @@ public class InternalClientRepository {
         internalClient2.setCompany("Sopra");
         internalClient2.setState("Pays de la loire");
         leadTOs.add(internalClient2);
+
+
+        ws_crm.InternalClient internalClient3 = new ws_crm.InternalClient();
+        internalClient3.setFirstAndLastName("julien, laffeli");
+        internalClient3.setAnnualRevenue(41000.0);
+        internalClient3.setPhone("04582569545");
+        internalClient3.setStreet("3 rue du champ de bataille");
+        internalClient3.setPostalCode("49000");
+        internalClient3.setCity("Angers");
+        internalClient3.setCountry("France");
+        internalClient3.setCreationDate(xmlGregorianCalendar);
+        internalClient3.setCompany("Sopra");
+        internalClient3.setState("Maine et loire");
+        leadTOs.add(internalClient3);
     }
 
     public List<ws_crm.InternalClient> findLeads(double lowAnnualRevenue, double highAnnualRevenue, String state) {
